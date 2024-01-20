4e6dbb5076b4ea255edf349c8f20999717e6ea8f
Hugo Hernandez Valdez <hugohv10@gmail.com>
2024-01-19 21:22:43 -0600

    fix: Migrations and models

---
+++ src/main/java/hangouh/me/medi/link/v1/models/Patient.java
@@ -0,0 +1,87 @@
+package hangouh.me.medi.link.v1.models;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "patient")
+public class Patient {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  @Column(name = "patient_id")
+  private UUID patientId;
+
+  @Column(name = "name")
+  private String name;
+
+  @Column(name = "date_of_birth")
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+  private Date dob;
+
+  @Column(name = "gender")
+  private char gender;
+
+  @Column(name = "contact_info")
+  private String contactInfo;
+
+  @Column(name = "created_at", nullable = false, updatable = false)
+  @CreationTimestamp
+  private Date createdAt;
+
+  @Column(name = "updated_at", nullable = false)
+  @UpdateTimestamp
+  private Date updatedAt;
+
+  @OneToOne(mappedBy = "patient", cascade = CascadeType.ALL, orphanRemoval = true)
+  @JsonManagedReference
+  private MedicalHistory medicalHistory;
+
+  @OneToMany(mappedBy = "patient", cascade = CascadeType.ALL, orphanRemoval = true)
+  @JsonManagedReference
+  private List<Prescription> prescriptions = new ArrayList<>();
+
+  @OneToMany(mappedBy = "patient", cascade = CascadeType.ALL, orphanRemoval = true)
+  @JsonManagedReference
+  private List<Appointment> appointments = new ArrayList<>();
+
+  public void setMedicalHistory(MedicalHistory medicalHistory) {
+    this.medicalHistory = medicalHistory;
+    if (medicalHistory != null && medicalHistory.getPatient() != this) {
+      medicalHistory.setPatient(this);
+    }
+  }
+
+  public void addPrescription(Prescription prescription) {
+    prescriptions.add(prescription);
+    if (prescription != null && prescription.getPatient() != this) {
+      prescription.setPatient(this);
+    }
+  }
+
+  public void addAppointment(Appointment appointment) {
+    appointments.add(appointment);
+    if (appointment.getPatient() != this) {
+      appointment.setPatient(this);
+    }
+  }
+}
