a1fff98add2e691a3d6bf6d0e20ca1da37928864
Hugo Hernandez Valdez <hugohv10@gmail.com>
2024-01-28 00:06:34 -0600

    fix: remove user role association

--- src/main/java/hangouh/me/medi/link/v1/models/User.java
+++ src/main/java/hangouh/me/medi/link/v1/models/User.java
@@ -31,7 +31,7 @@ public class User {
   @JsonIgnore
   private String password;
 
-  @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+  @ManyToMany(fetch = FetchType.EAGER, cascade = { CascadeType.PERSIST, CascadeType.MERGE })
   @JoinTable(
       name = "users_roles",
       joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "user_id"),
